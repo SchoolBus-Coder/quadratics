57c6faab9ed164f32167aca9fb4b051a7cb90961
Zac Miller <zahary.miller.2023@student.lhschools.org>
2019-09-26 10:31:19 -0400

    set up scanner

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -1,5 +1,7 @@
 package org.lickingheights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,11 +9,18 @@ public class Main {
         //todo declare variables a, b, c, discriminant, axisOfSymmetry,parabolaFacing, x, y
         double a, b, c, discriminant, axisOfSymmetry, x, y;
         String paraboloFacing;
-
+        Scanner radar;
         //todo initialize manually
-        a = 5;
-        b = 3;
-        c = 1;
+        radar = new Scanner(System.in);
+        System.out.println("Give me an A");
+        a = radar.nextDouble();
+        System.out.println("Give me an B");
+        b = radar.nextDouble();
+        System.out.println("Give me an C");
+        c = radar.nextDouble();
+//        a = 5;
+//        b = 3;
+//        c = 1;
         //todo do calculations
         axisOfSymmetry = -b / (2 * a);
         discriminant = (b * b) - 4 * (a * c);
@@ -19,14 +28,13 @@ public class Main {
             System.out.println("There are two real solutions");
         } else if (discriminant == 0)
             System.out.println("There is one real solution");
-    
+
         else{
             System.out.println("There are NO real solution");
         }
         //todo print results
         System.out.println("Axis of Symmetry: "+axisOfSymmetry);
         System.out.println("Discriminant: "+discriminant);
-        //todo not in code check results
 
 
         //todo initialize scanner and redo the initialize area
