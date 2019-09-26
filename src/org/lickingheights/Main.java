4543abcac2de22ececa2fabb65011eb4f4560611
Zac Miller <zahary.miller.2023@student.lhschools.org>
2019-09-26 10:24:01 -0400

    implemented if else ladder

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -5,38 +5,32 @@ public class Main {
     public static void main(String[] args) {
 
         //todo declare variables a, b, c, discriminant, axisOfSymmetry,parabolaFacing, x, y
-        double a,b,c,discriminant,axisOfSymmetry,x,y;
-                String paraboloFacing;
+        double a, b, c, discriminant, axisOfSymmetry, x, y;
+        String paraboloFacing;
 
         //todo initialize manually
-        a=5;
-        b=3;
-        c=1;
+        a = 5;
+        b = 3;
+        c = 1;
         //todo do calculations
-        axisOfSymmetry = -b/(2*a);
-        discriminant = (b*b)-4*(a*c);
-
+        axisOfSymmetry = -b / (2 * a);
+        discriminant = (b * b) - 4 * (a * c);
+        if (discriminant > 0) {
+            System.out.println("There are two real solutions");
+        } else if (discriminant == 0)
+            System.out.println("There is one real solution");
+    
+        else{
+            System.out.println("There are NO real solution");
+        }
         //todo print results
         System.out.println("Axis of Symmetry: "+axisOfSymmetry);
         System.out.println("Discriminant: "+discriminant);
         //todo not in code check results
-        
+
 
         //todo initialize scanner and redo the initialize area
 
         //todo double check results
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
